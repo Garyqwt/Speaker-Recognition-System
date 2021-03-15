9c27c38d8ad239439161fd8b3be36d5aa006d2ed
Garyqwt <40152033+Garyqwt@users.noreply.github.com>
2021-03-15 00:04:34 -0700

    Delete Mel_Spectrum.m

--- src/Mel_Spectrum.m
+++
@@ -1,9 +0,0 @@
-function  Mel_Spectrum_out = Mel_Spectrum(H,Mag_Sxk{1})
-
-for j=1:1:length(Mag_Sxk{1}(1,:))
-    for m = 1:1:length(H(:,1))
-            A = sum(Mag_Sxk{1}(:,j).*H(m,:)');
-    end
-        Mel_Spectrum_out(j,:) = A;
-end
-end
